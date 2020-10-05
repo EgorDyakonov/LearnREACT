5482fa6f7ba3bed6b0b2e0c57cd65a0dcd9f4a4d
EgorDyakonov <egor.dyakonov666@gmail.com>
2020-10-05 23:53:00 +0300

    add life cycle in Users.jsx

--- src/components/Users/users.jsx
+++ src/components/Users/users.jsx
@@ -4,9 +4,7 @@ import * as axios from "axios";
 import userPhoto from "./../../../src/assets/images/user.png";
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
-        
+    componentDidMount() {
         if(this.props.users.length === 0) {
             axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
                 this.props.setUsers(response.data.items);
